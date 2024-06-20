ddf7187b3fb383f0f23f7984731b3af835628807
Kausar-Dason <iddidasonkausar2002@gmail.com>
2024-06-20 16:18:16 -0700

    Fix Betty style issues in 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,16 @@
 /**
  * main - Entry point
  *
- * Description: This program prints "Programming is like building a multilingual puzzle"
- *              followed by a new line using the puts function.
+ * Description:
+ *    This program prints "Programming is like building a multilingual puzzle"
+ *    followed by a new line using the puts function.
  *
- * Return: Always 0 (Success)
+ * Return:
+ *    Always 0 (Success)
  */
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
+
