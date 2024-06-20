febdbc6801b2ede6340cd1c8ac0f239f7a58b3c4
Kausar-Dason <iddidasonkausar2002@gmail.com>
2024-06-20 15:55:10 -0700

    Write a C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  * main - Entry point
  *
  * Description: This program prints
- * 		"Programming is like building a multilingual puzzle"
- *              followed by a new line using the puts function.
+ * "Programming is like building a multilingual puzzle"
+ * followed by a new line using the puts function.
  *
  * Return: Always 0 (Success)
  */
