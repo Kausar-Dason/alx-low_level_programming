b94b28913d7ad2fe67405b53d43f4606d8e40d6c
Kausar-Dason <iddidasonkausar2002@gmail.com>
2024-06-22 08:26:23 -0700

    Printing the letters of the alphabet using the putchar function

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Description: Printing the letters of the alphbet 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++) {
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return 0;
+}
