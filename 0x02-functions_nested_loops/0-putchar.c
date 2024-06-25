ceda269b8e24ea5392b54a40c2ddcf8198937843
Kausar-Dason <iddidasonkausar2002@gmail.com>
2024-06-25 09:07:58 -0700

    Write a program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "_putchar.h"
+
+/**
+ * main - Prints _putchar followed by a new line
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+	
+	for (i = 0; i < 9; i++) 
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
