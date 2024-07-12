dd1ff3decee9c3bd1450873f9ce6055783732aa1
Kausar-Dason <iddidasonkausar2002@gmail.com>
2024-07-12 03:52:52 -0700

    Write a function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,7 +11,7 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 == '*')
 	{
 		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
-		{	
+		{
 			return (0);
 		}
 		if (*s2 == '*' && *(s2 + 1) == '\0')
@@ -23,7 +23,7 @@ int wildcmp(char *s1, char *s2)
 			return (wildcmp(s1, s2 + 1));
 		}
 		if (*s2 == '*' && wildcmp(s1 + 1, s2))
-		{	
+		{
 			return (1);
 		}
 		return (wildcmp(s1, s2 + 1));
