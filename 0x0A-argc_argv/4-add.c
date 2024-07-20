fb4c8a3b40fdfb9e57e5158aaf899d6b8f8b9967
Kausar-Dason <iddidasonkausar2002@gmail.com>
2024-07-20 10:17:18 -0700

    Write a program that adds positive numbers.

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  _isdigit - checks for a digit (0 through 9).
+ *  @c: variable that takes in numbers.
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
+ * main - a program that adds positive numbers.
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i, j;
+        
+	if (argc < 2)
+	{
+		printf("0\n");
+	}  
+         
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!_isdigit(argv[i][j]) )
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d", sum);   
+	printf("\n");
+
+	return (0);
+}
