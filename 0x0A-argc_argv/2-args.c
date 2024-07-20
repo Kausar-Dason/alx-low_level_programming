aa4cb1ae066db5bff514fcdd7376ebe96067286c
Kausar-Dason <iddidasonkausar2002@gmail.com>
2024-07-20 01:01:03 -0700

    Write a program that prints all arguments it receives.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all arguments it receives.
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (success)
+ */
+int main (int argc, char *argv[])
+{
+	int i;
+	
+	for ( i = 0; i < argc; i++)
+	{
+		printf("%s", argv[i]);
+		printf("\n");
+	}
+	printf("\n");
+
+	return (0);
+}	
