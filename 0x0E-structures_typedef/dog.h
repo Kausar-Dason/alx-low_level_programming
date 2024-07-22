58da94e170798d6c4624b203446c327979151904
Kausar-Dason <iddidasonkausar2002@gmail.com>
2024-07-22 13:09:23 -0700

    This structure is used to store information about a dog.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Represents a dog with a name, age, and owner
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: This structure is used to store info about a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
