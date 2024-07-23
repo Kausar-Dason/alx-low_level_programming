b528a71e0d4d763261b248f6d4f7ca192e93556e
Kausar-Dason <iddidasonkausar2002@gmail.com>
2024-07-22 17:51:13 -0700

    Write a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,22 +13,6 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
-	int name_len = 0, owner_len = 0, i;
-	char *new_name = NULL, *new_owner = NULL;
-
-	if (name == NULL || owner == NULL)
-	{
-		return (NULL);
-	}
-
-	while (name[name_len] != '\0')
-	{
-		name_len++;
-	}
-	while (owner[owner_len] != '\0')
-	{
-		owner_len++;
-	}
 
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
@@ -36,26 +20,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	new_owner = malloc(owner_len + 1);
-	if (new_owner == NULL)
-	{
-		free(new_name);
-		free(ptr);
-		return (NULL);
-	}
-
-	for (i = 0; i <= name_len; i++)
-	{
-		new_name[i] = name[i];
-	}
-	for (i = 0; i <= owner_len; i++)
-	{
-		new_owner[i] = owner[i];
-	}
-
-	ptr->name = new_name;
+	ptr->name = name;
 	ptr->age = age;
-	ptr->owner = new_owner;
+	ptr->owner = owner;
 
 	return (ptr);
 }
